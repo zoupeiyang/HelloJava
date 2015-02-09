22b3734e04370928934eef109d5de06735af5142
zoupeiyang <zoupeiyang@gmail.com>
2015-02-09 11:42:25 +0800

    modify

---
+++ src/com/sumszw/HelloJava.java
@@ -0,0 +1,14 @@
+package com.sumszw;
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Student");
+		System.out.println("Student");
+		System.out.println("Student");
+	}
+
+}
---
+++ src/com/sumszw/Student.java
@@ -0,0 +1,23 @@
+package com.sumszw;
+public class Student {
+
+	private String studentName;
+	private int age;
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
